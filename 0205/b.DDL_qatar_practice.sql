16db9ed73dc7b82be8b3783877ad1f6e297b6696
asi0000 <asi0000@nate.com>
2024-02-05 15:01:36 +0900

    240205 DML

--- 0205/b.DDL_qatar_practice.sql
+++ 0205/b.DDL_qatar_practice.sql
@@ -9,36 +9,43 @@
 
 -- 경기 (경기일정, 국가1, 국가2, 경기장, 주심, 선심, 경기결과)
 
+-- qatar_asian_cup
 CREATE DATABASE qatar_asian_cup;
 USE qatar_asian_cup;
+
+-- country (name[가변문자열 30], group_name[가변문자열 1], manager[가변문자열 30], lanking[정수])
 CREATE TABLE country(
 	name VARCHAR(30),
-    group VARCHAR(1),
+    group_name VARCHAR(1),
     manager VARCHAR(30),
-    lanking int
+    lanking INT
 );
 
+-- stadium (name[가변문자열 50], capacity[정수], address[가변문자열 255])
 CREATE TABLE stadium(
 	name VARCHAR(50),
-    capacity int,
+    capacity INT,
     address VARCHAR(255)
 );
 
+-- player (name[가변문자열 30], age[정수], position[가변문자열 15], pro_team[가변문자열 30], uniform_number[정수], country[가변문자열 30])
 CREATE TABLE player (
 	name VARCHAR(30),
-    age int,
+    age INT,
     position VARCHAR(15),
     pro_team VARCHAR(30),
-    uniform_number int,
+    uniform_number INT,
     country VARCHAR(30)
 );
 
+-- referee (name[가변문자열 30], age[정수], position[가변문자열 15])
 CREATE TABLE referee (
 	name VARCHAR(30),
-    age int,
+    age INT,
     position VARCHAR(15)
 );
 
+-- game (schedule[날짜및시간], country1[가변문자열 30], country2[가변문자열 30], stadium[가변문자열 50], chief_referee[가변문자열 30], second_referee[가변문자열 30], winning_country[가변문자열 30])
 CREATE TABLE game (
 	schedule DATETIME,
     country1 VARCHAR(30),
@@ -48,6 +55,3 @@ CREATE TABLE game (
     second_referee VARCHAR(30),
     winning_country VARCHAR(30)
 );
-
-
-
